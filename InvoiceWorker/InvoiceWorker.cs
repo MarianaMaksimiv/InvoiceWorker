5993705f8b2c63693d6a36c198df8dc007d7926c
t-perevernykhata <t.perevernykhata@xero.com>
2021-08-13 16:45:25 +1200

    Setup serialisation, add try/catch to worker

--- InvoiceWorker/InvoiceWorker.cs
+++ InvoiceWorker/InvoiceWorker.cs
@@ -1,11 +1,15 @@
+using System;
 using Microsoft.Extensions.Hosting;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using InvoiceWorker.Models;
+using Microsoft.Extensions.Logging;
 
 namespace InvoiceWorker
 {
@@ -13,25 +17,54 @@ namespace InvoiceWorker
     {
         private readonly HttpClient _httpClient;
         private readonly IInvoiceHandler _invoiceHandler;
+        private readonly IHostApplicationLifetime _appLifetime;
+        private readonly ILogger _logger;
 
-        public InvoiceWorker(HttpClient httpClient, IInvoiceHandler invoiceHandler)
+        public InvoiceWorker(HttpClient httpClient, IInvoiceHandler invoiceHandler, IHostApplicationLifetime appLifetime, ILoggerFactory loggerFactory)
         {
             _httpClient = httpClient;
             _invoiceHandler = invoiceHandler;
+            _appLifetime = appLifetime;
+            _logger = loggerFactory.CreateLogger<InvoiceWorker>();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                var response = await _httpClient.GetFromJsonAsync<InvoiceEventResponse>(await InvoiceApiSettings.GenerateUriAsync(), stoppingToken);
-                await InvoiceApiSettings.SaveHighwatermarkAsync(response.Items.Last().Id);
-
-                foreach (var invoiceEvent in response.Items)
+                try
                 {
-                    await _invoiceHandler.ProcessEventAsync(invoiceEvent);
+                    var response = await _httpClient.GetFromJsonAsync<InvoiceEventResponse>(await InvoiceApiSettings.GenerateUriAsync(), JsonSerializerOptions(), stoppingToken);
+                    if (!response.Items?.Any() ?? true)
+                    {
+                        _logger.LogInformation("Invoice Worker has not received Invoice data events. The application will exit.");
+                        _appLifetime.StopApplication();
+                        return;
+                    }
+
+                    await InvoiceApiSettings.SaveHighwatermarkAsync(response.Items.Last().Id);
+                    foreach (var invoiceEvent in response.Items)
+                    {
+                        await _invoiceHandler.ProcessEventAsync(invoiceEvent);
+                    }
+                }
+                catch (Exception e)
+                {
+                    _logger.LogCritical(e, "Invoice Worker fatal error. The application will exit.");
+                    _appLifetime.StopApplication();
                 }
             }
+        }
+
+        private static JsonSerializerOptions JsonSerializerOptions()
+        {
+            var options = new JsonSerializerOptions
+            {
+                IgnoreNullValues = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            options.Converters.Add(new JsonStringEnumConverter());
+            return options;
         }
     }
 
--- InvoiceWorker/Models/InvoiceEvent.cs
+++ InvoiceWorker/Models/InvoiceEvent.cs
@@ -5,8 +5,13 @@ namespace InvoiceWorker.Models
     public class InvoiceEvent
     {
         public long Id { get; set; }
-        public string Type { get; set; }
+        public EventType Type { get; set; }
         public Invoice Content { get; set; }
         public DateTime CreatedDateUtc { get; set; }
+    }
+
+    public enum EventType
+    {
+        INVOICE_CREATED
     }
 }
